737d13ab20d62096eba2dc21abcf916ff39fa41e
Easy-going12 <qntjf23@naver.com>
2025-08-20 20:09:40 +0900

    백준 챕터5 심화 3번까지 풀이

---
+++ chap01-input_and_calculate/src/main/java/problem3_1001.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class problem3_1001 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.print(a-b);
+    }
+}
---
+++ chap01-input_and_calculate/src/main/java/problem5_1008.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class problem5_1008 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+
+        System.out.print(a/b);
+    }
+}
