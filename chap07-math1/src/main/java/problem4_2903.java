0c981d3d8fd50d540c74c9fd88d0df501e2e6f9d
Easy-going12 <qntjf23@naver.com>
2025-09-01 19:14:25 +0900

    백준 7단원 4문제까지 풀이

--- chap07-math1/src/main/java/problem4_2903.java
+++ chap07-math1/src/main/java/problem4_2903.java
@@ -1,8 +1,18 @@
 import java.io.BufferedReader;
-import java
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class problem4_2903 {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        double result = 2;
+
+        for(int i  = 0; i < num; i++){
+            result += Math.pow(2,i);
+        }
+
+        System.out.println((int)(result * result));
     }
 }
---
+++ chap07-math1/src/main/java/problem5_2292.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+
+public class problem5_2292 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        int count = 1;
+
+        for (int i = 0; i < num; i++) {
+            for(int j = 0; j< num; j++){
+
+            }
+            
+        }
+    }
+}
